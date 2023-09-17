fbd49dd57c2f50706549dd7c7f75815759faee4e
darshil89 <darshilmahraur67@gmail.com>
2023-09-17 15:18:43 +0530

    project completed

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,25 @@ export default function Home() {
   const onSubmit = async (e) => {
     e.preventDefault();
     console.log(file);
+    const data = new FormData();
+    data.set("file", file);
+    const res = await fetch("/api/upload", {
+      method: "POST",
+      body: data,
+    });
+    const result = await res.json();
+    if (result.error) {
+      alert(result.error);
+    }
+    if (result.message) {
+      alert(result.message);
+    }
   };
   return (
     <>
       <div className="flex flex-col justify-center align-center w-full h-screen">
         <h1 className="text-3xl font-bold mb-5">Upload Image</h1>
-        <form onSubmit={onSubmit} >
+        <form onSubmit={onSubmit}>
           <input
             type="file"
             name="file"
